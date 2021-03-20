d294d883abfe64d77f0fc3f1f84dfa25a6b60a35
Drew Overcash <30905686+djovercash@users.noreply.github.com>
2021-03-20 18:16:07 -0400

    General Cleanup (#13)
    Co-authored-by: Overcash <dovercash@forbes.com>

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import TrackButton from '../components/TrackButton';
 
-const TrackList = ({ currentNavView = 'audio', currentTracks = [], currentTrackIndex = null, isPaused = false, viewCallback = null, trackChangeCallback = null }) => {
+const TrackList = ({ currentNavView = 'audio', currentTracks = [], currentTrackIndex = null, isPaused = false, navCallback = null, trackChangeCallback = null }) => {
 	const mediums = ['audio', 'video'];
 
 	return (
 		<div className='track-list'>
 			<div className='track-list__nav'>
 				{mediums.map((medium, key) =>
-					<div className={`track-list__nav--item track-list__nav--item-${medium}`} key={key} data-active={medium === currentNavView} onClick={() => viewCallback(medium)}>{medium}</div>
+					<div className={`track-list__nav--item track-list__nav--item-${medium}`} key={key} data-active={medium === currentNavView} onClick={() => navCallback(medium)}>{medium}</div>
 				)}
 			</div>
 			<div className='track-list__tracks'>
